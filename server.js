1cd71994c685f3f89539d625b43bd94d1f9ee006
Stephen Moore <judo2000@comcast.net>
2022-01-18 22:28:07 -0700

    Create function to view all emmployees

--- server.js
+++ server.js
@@ -23,7 +23,20 @@ const options = () => {
     ])
     .then(response => {
         if (response.action === 'View All Employees') {
-            console.log('Vide All Employees');
+            viewEmployees();
         }
     });
 };
+
+const viewEmployees = async () => {
+    try {
+        const empSQL = "SELECT employee.firstName AS 'First Name', employee.lastName AS 'Last Name', role.title AS Title, role.salary AS Salary, department.name AS Department FROM employee INNER JOIN role ON employee.role_id = role.id JOIN department on role.department_id = department.id;";
+        const [ employees ] = await connection.query(empSQL);
+        console.log('-----View All Employees-----\n')
+        console.table(employees);
+        options();
+    } catch (error) {
+        console.log(error);
+    }
+};
+options();
