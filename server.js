9f93bd1f09bed70c99c192b5c6eb791c5c064f30
Stephen Moore <judo2000@comcast.net>
2022-01-18 23:03:30 -0700

    Create function to view all roles and add and update schema.sql file

--- server.js
+++ server.js
@@ -23,20 +23,46 @@ const options = () => {
     ])
     .then(response => {
         if (response.action === 'View All Employees') {
-            viewEmployees();
+            viewAllEmployees();
+        } else if (response.action === 'Add Employee') {
+            console.log('Add Enployee')
+        } else if (response.action === 'Update Employee Role') {
+           console.log('Update Employee Role');
+        } else if (response.action === 'View All Roles') {
+            viewAllRoles();
+        } else if (response.action === 'Add a Role') {
+            console.log('Add a Role');
+        } else if (response.action === 'View All Departments') {
+            console.log('View All Departments')
+        } else if (response.action === 'Add a Department') {
+            console.log('Add a Department')
+        } else if (response.action === 'Exit') {
+            console.log('Exit')
         }
     });
 };
 
-const viewEmployees = async () => {
+const viewAllEmployees = async () => {
     try {
         const empSQL = "SELECT employee.firstName AS 'First Name', employee.lastName AS 'Last Name', role.title AS Title, role.salary AS Salary, department.name AS Department FROM employee INNER JOIN role ON employee.role_id = role.id JOIN department on role.department_id = department.id;";
         const [ employees ] = await connection.query(empSQL);
-        console.log('-----View All Employees-----\n')
+        console.log('\n-----View All Employees-----\n')
         console.table(employees);
         options();
     } catch (error) {
         console.log(error);
     }
 };
+
+const viewAllRoles = async () => {
+    try {
+        const rolesSQL = "SELECT role.title AS 'Title', role.salary AS 'Salary', department.name AS 'Department' FROM role JOIN department on role.department_id = department.id;";
+        const [ roles ] = await connection.query(rolesSQL);
+        console.log('\n-----View All Roles-----\n');
+        console.table(roles);
+        options();
+    } catch (error) {
+        console.error(error);
+    } 
+}
 options();
---
+++ shemas/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
